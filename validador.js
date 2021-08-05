6e9d257acd0315dfac2dd685449b1d1729bc50ed
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-08-05 16:16:06 -0300

    finish

--- validador.js
+++ validador.js
@@ -17,12 +17,36 @@ function validaCPF(cpf) {
         }
 
         console.log(soma);
+
+        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        
+        //validação do primeiro digito
+        if(resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+        
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+        
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+        
+        //validação segundo digito
+        if(resultado != digitos.charAt(1)) {
+            return false
+        }
+
         return true;
     }
 }
 
 function validacao() {
     console.log("Iniciando validação CPF");
+    document.getElementById('success').style.display = 'none'
+    document.getElementById('error').style.display = 'none'
     
     var cpf = document.getElementById('cpf_digitado').value;
     
